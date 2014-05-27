964285d53b8e41ca4158b187db2e88ef72353929
Joakim Børlum <joakim@roevhat.dk>
2014-05-27 11:29:50 +0200

    Lovely slideshow

--- js/index.js
+++ js/index.js
@@ -2,29 +2,39 @@ var roegfest = {
     clientID: '0d3ab0f612d1463ba64e094d7f0f5b46',
     tag: 'roegfest2014',
     instagramAPI: 'https://api.instagram.com/v1/',
+    imageContainer: $('#image'),
+
+    data: [],
+
+    index: 0,
 
     init: function() {
-        this.update();
+        this.update(roegfest.slide);
+
         window.setInterval(function() {
-            roegfest.update();
+            roegfest.update(roegfest.slide);
         }, 5000);
     },
 
-    update: function() {
+    update: function(callback) {
         NProgress.start();
         var url = this.instagramAPI + 'tags/' + this.tag
             + '/media/recent?client_id=' + this.clientID;
 
         var onSuccess = function(resp) {
-            console.log(resp);
-            var newestURL = resp.data[0].images.standard_resolution.url;
-            $('#image').css('background-image', 'url(' + newestURL + ')');
+            roegfest.data = resp.data;
             NProgress.done();
+            if (typeof callback === 'function') {
+                callback.apply(roegfest);
+            }
         };
 
         var onFail = function(resp) {
             console.log('Failed to load data');
             NProgress.done();
+            if (typeof callback === 'function') {
+                callback.apply(roegfest);
+            }
         };
 
         $.ajax({
@@ -34,5 +44,17 @@ var roegfest = {
             url: url,
             success: onSuccess
         });
+    },
+
+    slide: function() {
+        this.imageContainer.hide();
+        var currentURL = this.data[this.index].images.standard_resolution.url;
+        this.imageContainer.css('background-image', 'url(' + currentURL + ')');
+        if (this.index == this.data.length-1 || this.index == 4) {
+            this.index = 0;
+        } else {
+            this.index++;
+        }
+        this.imageContainer.show();
     }
 };
